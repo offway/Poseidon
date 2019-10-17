3d6d5174eb54f425683752eccfcf247c01e17fef
XiaZhengXin <xzx@xiazhengxin.name>
2019-10-17 17:54:06 +0800

    格式化、规范并引入头部 公共类。。common.js.. 同时引入 datatable 库文件。。。
    Signed-off-by: XiaZhengXin <xzx@xiazhengxin.name>

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -22,38 +22,36 @@ function post(URL, PARAMS) {
 //(new Date()).Format("yyyy-MM-dd hh:mm:ss.S") ==> 2006-07-02 08:09:04.423
 //(new Date()).Format("yyyy-M-d h:m:s.S")      ==> 2006-7-2 8:9:4.18
 Date.prototype.Format = function (fmt) { //author: meizz
- var o = {
-     "M+": this.getMonth() + 1,                 //月份
-     "d+": this.getDate(),                    //日
-     "h+": this.getHours(),                   //小时
-     "m+": this.getMinutes(),                 //分
-     "s+": this.getSeconds(),                 //秒
-     "q+": Math.floor((this.getMonth() + 3) / 3), //季度
-     "S": this.getMilliseconds()             //毫秒
- };
- if (/(y+)/.test(fmt))
-     fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
- for (var k in o)
-     if (new RegExp("(" + k + ")").test(fmt))
-         fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
- return fmt;
-}
+    var o = {
+        "M+": this.getMonth() + 1,                 //月份
+        "d+": this.getDate(),                    //日
+        "h+": this.getHours(),                   //小时
+        "m+": this.getMinutes(),                 //分
+        "s+": this.getSeconds(),                 //秒
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度
+        "S": this.getMilliseconds()             //毫秒
+    };
+    if (/(y+)/.test(fmt))
+        fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+        if (new RegExp("(" + k + ")").test(fmt))
+            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+};
 
 
 //格式化金额
-function fmoney(s, n)
-{
-	var num = n>0?s.toFixed(n):s;
-	return (s+"").replace(/(\d{1,3})(?=(\d{3})+(?:$|\.))/g,'$1,');
-  
+function fmoney(s, n) {
+    var num = n > 0 ? s.toFixed(n) : s;
+    return (s + "").replace(/(\d{1,3})(?=(\d{3})+(?:$|\.))/g, '$1,');
 }
 
 //用于生成uuid
 var UUID = {
-	S4:function () {
-        return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+    S4: function () {
+        return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
     },
-    randomUUID: function() {
-        return (UUID.S4()+UUID.S4()+"-"+UUID.S4()+"-"+UUID.S4()+"-"+UUID.S4()+"-"+UUID.S4()+UUID.S4()+UUID.S4());
+    randomUUID: function () {
+        return (UUID.S4() + UUID.S4() + "-" + UUID.S4() + "-" + UUID.S4() + "-" + UUID.S4() + "-" + UUID.S4() + UUID.S4() + UUID.S4());
     }
-}
+};
