c2468ba5b884b20e12dc4051e488e09fa3db1a5d
Tubowen <tubowen@puhao-art.com>
2019-10-22 11:38:56 +0800

    添加电子刊模板2

---
+++ src/main/java/cn/offway/Poseidon/controller/Template2Controller.java
@@ -0,0 +1,30 @@
+package cn.offway.Poseidon.controller;
+
+import cn.offway.Poseidon.properties.QiniuProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 用户管理
+ *
+ * @author wn
+ */
+@Controller
+public class Template2Controller {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private QiniuProperties qiniuProperties;
+
+    @RequestMapping("/template2.html")
+    public String users(ModelMap map) {
+        map.addAttribute("qiniuUrl", qiniuProperties.getUrl());
+        return "template2";
+    }
+
+}
