c902f9ce8dc82cd5cd139485364295cd6cd33499
XiaZhengXin <xzx@xiazhengxin.name>
2019-10-28 12:18:22 +0800

    商品（杂志）数据CRUD接口。。。。。
    Signed-off-by: XiaZhengXin <xzx@xiazhengxin.name>

--- src/main/java/cn/offway/Poseidon/service/PhTemplateService.java
+++ src/main/java/cn/offway/Poseidon/service/PhTemplateService.java
@@ -4,6 +4,8 @@ package cn.offway.Poseidon.service;
 import java.util.List;
 
 import cn.offway.Poseidon.domain.PhTemplate;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 /**
  * Service接口
@@ -11,13 +13,15 @@ import cn.offway.Poseidon.domain.PhTemplate;
  * @author wn
  * @version $v: 1.0.0, $time:2019-10-21 14:46:27 Exp $
  */
-public interface PhTemplateService{
+public interface PhTemplateService {
 
     PhTemplate save(PhTemplate phTemplate);
-	
+
     PhTemplate findOne(Long id);
 
     void delete(Long id);
 
     List<PhTemplate> save(List<PhTemplate> entities);
+
+    Page<PhTemplate> list(Pageable pageable);
 }
--- src/main/java/cn/offway/Poseidon/service/impl/PhTemplateServiceImpl.java
+++ src/main/java/cn/offway/Poseidon/service/impl/PhTemplateServiceImpl.java
@@ -1,15 +1,16 @@
 package cn.offway.Poseidon.service.impl;
 
-import java.util.List;
-
+import cn.offway.Poseidon.domain.PhTemplate;
+import cn.offway.Poseidon.repository.PhTemplateRepository;
+import cn.offway.Poseidon.service.PhTemplateService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import cn.offway.Poseidon.service.PhTemplateService;
 
-import cn.offway.Poseidon.domain.PhTemplate;
-import cn.offway.Poseidon.repository.PhTemplateRepository;
+import java.util.List;
 
 
 /**
@@ -21,28 +22,33 @@ import cn.offway.Poseidon.repository.PhTemplateRepository;
 @Service
 public class PhTemplateServiceImpl implements PhTemplateService {
 
-	private Logger logger = LoggerFactory.getLogger(this.getClass());
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-	@Autowired
-	private PhTemplateRepository phTemplateRepository;
-	
-	@Override
-	public PhTemplate save(PhTemplate phTemplate){
-		return phTemplateRepository.save(phTemplate);
-	}
-	
-	@Override
-	public PhTemplate findOne(Long id){
-		return phTemplateRepository.findOne(id);
-	}
+    @Autowired
+    private PhTemplateRepository phTemplateRepository;
 
-	@Override
-	public void delete(Long id){
-		phTemplateRepository.delete(id);
-	}
+    @Override
+    public PhTemplate save(PhTemplate phTemplate) {
+        return phTemplateRepository.save(phTemplate);
+    }
 
-	@Override
-	public List<PhTemplate> save(List<PhTemplate> entities){
-		return phTemplateRepository.save(entities);
-	}
+    @Override
+    public PhTemplate findOne(Long id) {
+        return phTemplateRepository.findOne(id);
+    }
+
+    @Override
+    public void delete(Long id) {
+        phTemplateRepository.delete(id);
+    }
+
+    @Override
+    public Page<PhTemplate> list(Pageable pageable) {
+        return phTemplateRepository.findAll(pageable);
+    }
+
+    @Override
+    public List<PhTemplate> save(List<PhTemplate> entities) {
+        return phTemplateRepository.save(entities);
+    }
 }
